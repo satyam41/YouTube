f5778e8d2fd4726f3e19ce32f1110a7bc07b5fa4
Satyam Krishna <thesatyam10@gmail.com>
2021-05-25 17:22:38 +0530

    Check

--- factor.py
+++ factor.py
@@ -1,15 +1,15 @@
-# # Factorial of number.
+# Factorial of number.
 
-# lst = []
-# num = int(input("Enter any number: "))
-# for i in range(1,num):
-#     if num%i==0:
-#         lst.append(i)
-# print(lst)
-
-
-lst = [1,2,3,4,5]
+lst = []
+num = int(input("Enter any number: "))
+for i in range(1,num):
+    if num%i==0:
+        lst.append(i)
 print(lst)
-for i in lst:
-    i = i*2
-    print(i)
+
+
+# lst = [1,2,3,4,5]
+# print(lst)
+# for i in lst:
+#     i = i*2
+#     print(i)
