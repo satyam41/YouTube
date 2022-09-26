a47e612539e9e894a0f7dca894adfc0032ca4c88
Satyam Krishna <63470846+satyam41@users.noreply.github.com>
2022-09-26 12:43:32 +0530

    Update Binary_Search.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -12,11 +12,11 @@ def Bsearch(x,n):
             beg = mid+1
         elif (n<x[mid]):
             end = mid-1
-        return flag
-    lst = [1,2,34,2,452,5]
-    lst.sort()
-    item = eval(input("Enter what you have to search?..."))
-    if Bsearch(lst, item):
-        print("Data Found")
-    else:
-        print("No Data Found")
+    return flag
+lst = [1,2,34,2,452,5]
+lst.sort()
+item = eval(input("Enter what you have to search?..."))
+if Bsearch(lst, item):
+    print("Data Found")
+else:
+    print("No Data Found")
